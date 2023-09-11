3edcd66893bf92967cef703d4e7fff00ddea4459
qkrgPals <phk200440@gmail.com>
2023-09-11 17:51:07 +0900

    파이썬일짱

--- seseo.py
+++ seseo.py
@@ -35,13 +35,14 @@ print(my_set) """
 """ my_set.discard(2)
 print(my_set) """
 
+
 #타입 변환
-my_int = 10
+#my_int = 10
 """ my_str = str(my_int)
 print(my_int)
 print(my_str) """
 
-print(my_int)
+""" print(my_int)
 
 print(my_int + 10)
 my_str = str(my_int)
@@ -50,4 +51,112 @@ print(my_str)
 
 #print(my_str + 10)
 
-print(my_str + "hello")
+print(my_str + "hello") """
+
+""" my_str = '10'
+print(my_str)
+
+my_int = int(my_str)
+print(my_int)
+
+print(my_int + 10)
+
+my_int2 = int("ten")
+print(my_int2) """
+
+
+#산술 연산자
+""" a = 10
+b = 3
+
+# c = a + b
+# c = a - b
+# c = a / b
+# c = a * b
+# c = a % b
+# c = a // b
+c = a ** b
+
+print(c)
+"""
+""" a = 0
+print(a)
+
+a += 2
+print(a)
+
+a -= 1
+print(a)
+
+a *= 4
+print(a)
+
+a /= 2
+print(a)
+
+a **= 3
+print(a) """
+
+
+#비교, 관계 연산자
+""" a = 10
+b = 4
+
+c = a > b    
+c = a < b    
+c = a >= b   
+c = a <= b   
+c = a == b   
+c = a != b #!는 서로 다르냐는 의미
+
+print(c)
+
+print(a > b)    
+print(a < b)    
+print(a >= b)   
+print(a <= b)   
+print(a == b)   
+print(a != b) """
+
+
+#논리 연산자
+""" a = 1
+b = 0
+
+print(a and b)
+print(a or b)
+print(not b)
+
+x = True
+y = False
+
+print(x and y)
+print(x or y)
+print(not x)
+print(not y) """
+
+
+#비트 연산자
+""" a = 10
+b = 3
+
+c = a & b
+c = a | b
+c = a ^ b
+c = ~ a
+c = a << b
+
+print(c)
+
+c = a >> 2
+
+print(c) """
+
+
+#멤버 연산자
+my_list = [9, 4, 3, 7, 8, 'hi']
+
+print(2 not in my_list)
+
+my_dic = {"key1" : "v1", "k2" : "v2"}
+print("k" in my_dic)
