f647e24a4968c2de4a9ee05f4408bd0d0ede1e35
qkrgPals <phk200440@gmail.com>
2023-09-11 16:47:52 +0900

    파이썬일짱

---
+++ seseo.py
@@ -0,0 +1,53 @@
+#집합(Set)
+""" my_set = {1, 2, 3, 4, 5}
+setItem = {5, 3, 1}
+print (my_set)
+print (setItem) """
+
+""" my_set = {5, 8, 3, 7, 1, "h"}
+print (my_set)
+print (*my_set) """
+
+""" my_set = {[5, 8, 3, 7, 1, "h"]}
+print (my_set) """
+
+""" set_tmp = set("hello")
+print(set_tmp) """
+
+""" my_set = {5, 8, 3, 7, 1, "h"}
+set_item = {7, 8, 4, 2, "h"} """
+#print(my_set | set_item)
+#print(my_set.union(set_item))
+
+#print(my_set - set_item)
+#print(my_set.difference(set_item))
+
+#print(my_set & set_item)
+#print(my_set.intersection(set_item))
+
+#print(my_set)
+#my_set.add(9)
+""" my_set.update([5, 9, 7, 4])
+print(my_set) """
+
+#my_set.clear()
+#my_set.remove(5)
+""" my_set.discard(2)
+print(my_set) """
+
+#타입 변환
+my_int = 10
+""" my_str = str(my_int)
+print(my_int)
+print(my_str) """
+
+print(my_int)
+
+print(my_int + 10)
+my_str = str(my_int)
+
+print(my_str)
+
+#print(my_str + 10)
+
+print(my_str + "hello")
