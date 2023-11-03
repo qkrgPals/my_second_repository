527a56a980a1af4b670baf719a96c9b275ed6116
qkrgPals <phk200440@gmail.com>
2023-11-03 17:47:12 +0900

    10_2주차

--- calc.py
+++ calc.py
@@ -13,3 +13,19 @@ def mul(a, b) :
 def div(a, b) :
     print("a / b : ", a / b)
     return a / b
+
+#------------------------------------------------------------
+
+#10_2주차
+class Calc:
+    def add(self, a, b):
+        return a + b
+    
+    def sub(self, a, b):
+        return a - b
+    
+    def mul(self, a, b):
+        return a * b
+    
+    def div(self, a, b):
+        return a / b
